d827150835b0e8e5d505523d6c5f031da2c56fd0
dahonegger <dahonegger@gmail.com>
2017-06-16 13:59:39 -0400

    Create test script
    Development of method test script

---
+++ frontTransect/testscript.m
@@ -0,0 +1,123 @@
+addpath(genpath(fileparts(pwd)))
+cubeDir = 'D:\DAQ-data\';
+
+files = getFiles(cubeDir);
+
+cubeName = [files(1).folder,files(1).name];
+
+load(cubeName,'Rg','Azi','timex','timeInt','results')
+if ~exist('timex','var') || isempty(timex)
+    load(cubeName,'data')
+    timex = mean(data,3);
+end
+
+%% Rectify to x-y
+[AZI,RG] = meshgrid(90-Azi-results.heading,Rg);
+[xrad,yrad] = pol2cart(AZI*pi/180,RG);
+xutm = xrad + results.XOrigin;
+yutm = yrad + results.YOrigin;
+[lat,lon] = UTMtoll(yutm,xutm,18);
+
+%% Create transect waypoints
+clickFig = figure;
+    hp = pcolor(lon,lat,timex);
+        shading interp
+        colormap(hot)
+        caxis([0 225])
+[lonwp,latwp] = ginput(2);
+
+%% Convert waypoints into transect
+[yutmwp,xutmwp] = lltoUTM(latwp,lonwp);
+xwp = xutmwp - results.XOrigin;
+ywp = yutmwp - results.YOrigin;
+s = hypot(diff(xwp),diff(ywp));
+stx = 0:10:s;
+xtx = interp1([0 s],xwp,stx);
+ytx = interp1([0 s],ywp,stx);
+
+txFig = figure;
+    hp = pcolor(xrad,yrad,timex);
+        shading interp
+        colormap(hot)
+        caxis([0 225])
+    hold on
+    plot(xtx,ytx,'.c')
+
+%% Convert transect points to polar
+[thtx,rgtx] = cart2pol(xtx,ytx);
+azitx = mod(90-thtx*180/pi-results.heading,360);
+
+polfig = figure;
+    hp = imagesc(Azi,Rg,timex);
+    colormap(hot)
+    caxis([0 225])
+    hold on
+    plot(azitx,rgtx,'.c')
+    
+%% Create gridded interpolant
+minRange = find(diff(Rg),1,'first');
+Rg_grid = Rg(minRange:end);
+
+
+[AZI_grid,RG_grid] = ndgrid(Azi,Rg_grid);
+gInt = griddedInterpolant(AZI_grid,RG_grid,single(timex(minRange:end,:)'));
+
+%% Interpolate to transect
+Itx = gInt(azitx,rgtx);
+
+txPlot = figure;
+    hp = plot(stx,Itx,'-k');
+    
+%% Loop thru files
+
+Itx = [];%nan(length(files),length(stx));
+dn = [];%nan(length(files),1);
+for i = 1:length(files)
+    fprintf('%d of %d.',i,length(files))
+    load([files(i).folder,files(i).name],'timeInt','timex','header')
+    if header.rotations > 64
+        fprintf('Long run.Rot:')
+        % Deal with long run
+        load([files(i).folder,files(i).name],'data')
+        fdata = movmean(data,32,3);
+        for j = 32:64:header.rotations-32
+            fprintf('%d.',j)
+            thisFrame = fdata(minRange:end,:,j);
+            dn = [dn epoch2Matlab(timeInt(1,j))];
+            gInt.Values = single(thisFrame');
+            Itx = [Itx;gInt(azitx,rgtx)];
+        end
+    else
+        dn = [dn epoch2Matlab(mean(timeInt(:)))];
+        
+        gInt.Values = single(timex(minRange:end,:)');
+        Itx = [Itx;gInt(azitx,rgtx)];
+    end
+    fprintf('\n')
+end
+
+%% Plot stack
+
+stackFig = figure;
+    hp = pcolor(stx,dn,Itx);
+        shading interp
+        datetick('y','keeplimits')
+    
+%% Compute gradient
+
+[dIds,dIdn] = gradient(Itx);
+DI = hypot(dIds,dIdn);
+
+gradientFig = figure;
+    hp = pcolor(stx,dn,log(abs(dIds)));
+        shading interp
+        datetick('y','keeplimits')
+
+%% Find front location
+
+for i = 1:length(dn)
+    % First point
+    if i==1
+        [minval,idx] = min
+    
+    
