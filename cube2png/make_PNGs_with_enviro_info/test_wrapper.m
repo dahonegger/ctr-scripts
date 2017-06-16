337593e0c0841c217b38fb6cc53e9c1622140638
simpsale <simpsale@oregonstate.edu>
2017-06-16 09:36:51 -0400

    New features enviro info plots
    Added flags to change colorbars for days with uniform brightness increase (colorbar still not perfect values, change for images BEFORE failure)

--- cube2png/make_PNGs_with_enviro_info/test_wrapper.m
+++ cube2png/make_PNGs_with_enviro_info/test_wrapper.m
@@ -3,7 +3,13 @@ close all; clear all;
 addpath(genpath('C:\Data\CTR\ctr-scripts')) %github path
 addpath(genpath('E:\SupportData')) %support data on CTR HUB
 
-cubeFile='D:\Data\CTR\DAQ-data\processed\2017-06-10\LyndePt_20171610000_pol.mat';
+
+% cubeFile='E:\DAQ-data\processed\2017-05-19\LyndePt_20171390000_pol';%before fail %works
+% cubeFile='E:\DAQ-data\processed\2017-05-27\LyndePt_20171470315_pol';%during fail %BAD
+% cubeFile='E:\DAQ-data\processed\2017-05-28\LyndePt_20171480000_pol';%during fail %ok
+% cubeFile='E:\DAQ-data\processed\2017-05-30\LyndePt_20171500941_pol';%during mega fail %works
+cubeFile='E:\DAQ-data\processed\2017-06-10\LyndePt_20171610731_pol';%after fail
+
 timexFile= 'D:\Data\CTR\postprocessed\timex_tides\LyndePt_20171610000_pol_timex.png';
 
 cube2timex_tides(cubeFile,timexFile)
