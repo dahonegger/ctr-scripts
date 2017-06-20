60bd77abe518e0c275501bd541a3c0be95e6913c
dahonegger <dahonegger@gmail.com>
2017-06-20 14:19:11 -0400

    Add local cornfield point tide extractor

---
+++ getTideInfo/cornfieldPointCurrentLocal.m
@@ -0,0 +1,8 @@
+function [yTide,dnTide] = cornfieldPointCurrentLocal
+
+fname = fullfile(fileparts(mfilename('fullpath')),'tideLocal','xtide_cornfieldPoint3miS_currents.txt');
+
+[yTide,dnTide] = loadXTide(fname);
+
+conversion = 0.5144; %1 naut = 0.5144 m/s
+yTide = yTide.*conversion;
