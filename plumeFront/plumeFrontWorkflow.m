fafdafa45b7a7d7dce579ae52bce9c8d2e0b5fb8
dahonegger <dahonegger@gmail.com>
2017-06-22 21:54:01 -0400

    plume front diffs + FFT addition

--- plumeFront/plumeFrontWorkflow.m
+++ plumeFront/plumeFrontWorkflow.m
@@ -5,7 +5,7 @@ else
     savePath = fullfile('C:','Data','CTR','plumeFront');
     scrDir = fullfile('C:','Data','CTR','ctr-scripts');
     cubeDir = fullfile('E:','DAQ-data','processed');
-    ebbNum = 72;
+    ebbNum = 74; % 74: 2017-June-22 0600
 end
 
 %% LABEL
